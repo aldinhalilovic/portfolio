70ce7bac2cde004f2324f20f0d5a8c7355602622
aldinhalilovic <aldinhalilovic06@gmail.com>
2024-07-18 12:17:43 +0200

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,24 +8,25 @@ import {
   TechName,
   ContactWrapper,
 } from "./AboutElements";
+import ManIcon from "../../Assets/manIlustration.webp";
+
 function About() {
   return (
     <ContactWrapper id="about">
       <div className="Container">
         <div className="SectionTitle">About Me</div>
         <div className="BigCard">
-          <Image
-            src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/man-svgrepo-com.svg"
-            alt="man-svgrepo"
-          />
+          <Image src={ManIcon} alt="man-svgrepo" />
           <div className="AboutBio">
-            Hello! My name is <strong>Your Name</strong> Lorem Ipsum is simply
-            dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s, when
-            an unknown printer took a galley of type and scrambled it to make a
-            type specimen book. It has survived not only five centuries, but
-            also the leap into electronic typesetting, remaining essentially
-            unchanged.
+            Hello! My name is <strong>Aldin Halilovic</strong>, passionate
+            Software Engineer specializing in frontend development. With a
+            strong background in creating dynamic and engaging user interfaces,
+            I thrive on transforming complex ideas into seamless, intuitive
+            experiences. I take pride in writing clean, efficient code and
+            continuously seek opportunities to learn and grow in the
+            ever-evolving tech landscape. When I'm not coding, you can find me
+            exploring the latest trends in car industry, or enjoying a good
+            movie. Let's connect and create something amazing together!
           </div>
           <div className="AboutBio tagline2">
             I have become confident using the following technologies.
